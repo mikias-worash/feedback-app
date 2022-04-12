f88996a7fc9d2cc7a8ab5ad368616081c3f51723
Mikias <mikias.worash.a@gmail.com>
2022-04-12 15:18:21 +0300

    Card component and Conditional styling

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 function FeedbackList({feedback}) {
     if (!feedback || feedback.length === 0)
@@ -9,6 +10,16 @@ function FeedbackList({feedback}) {
         ))}
     </div>
   )
+}
+
+FeedbackList.propTypes = {
+    feedback: PropTypes.arrayOf(
+        PropTypes.shape({ 
+            id: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+            rating: PropTypes.number.isRequired,
+        })
+    )
 }
 
 export default FeedbackList
