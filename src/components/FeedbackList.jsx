110f1f3bef347073b6c3fffac8a4e91639070df8
Mikias <mikias.worash.a@gmail.com>
2022-04-13 15:22:24 +0300

    Feedback Status component

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,16 +3,17 @@ import FeedbackItem from "./FeedbackItem"
 function FeedbackList({feedback, handleDelete}) {
     if (!feedback || feedback.length === 0)
         return <p>No feedback</p>
-  return (
-    <div className='feedback-list'>
-        {feedback.map((item) => (
-            <FeedbackItem 
-            key={item.id} 
-            item={item} 
-            handleDelete={handleDelete}/>
-        ))}
-    </div>
-  )
+        
+    return (
+        <div className='feedback-list'>
+            {feedback.map((item) => (
+                <FeedbackItem 
+                key={item.id} 
+                item={item} 
+                handleDelete={handleDelete}/>
+            ))}
+        </div>
+    )
 }
 
 FeedbackList.propTypes = {
