2ae9f754f40f4836868e0811fb33c15bfc5407e9
Mikias <mikias.worash.a@gmail.com>
2022-04-13 17:31:37 +0300

    Add Feedback Functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { v4 as uuidv4} from 'uuid'
 import { useState } from "react";
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
     const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure?')) {
+        if(window.confirm('Are you sure want to delete?')) {
             setFeedback(feedback.filter((item) => item.id !== id ))
         }
     }
@@ -14,8 +22,11 @@ function App() {
     return (
         <>
             <Header />
-            <div>
-                <FeedbackList feedback = {feedback} handleDelete={deleteFeedback} />
+            <div className='container'>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback = {feedback}/>
+                <FeedbackList feedback = {feedback}
+                 handleDelete={deleteFeedback} />
             </div>
         </>
     )
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,25 +1,54 @@
 import { useState } from "react"
 import Card from "./shared/Card"
 import Button from "./shared/Button"
+import RatingSelect from "./RatingSelect"
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text.trim().length > 0 && text.trim().length <= 10) {
+      setMessage('Review must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage()
+      setBtnDisabled(false)
+    }
     setText(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating
+      }
+
+      handleAdd(newFeedback)
+      setText('')
+    }
   }
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        {/* @todo - rating select component */}
+        <RatingSelect select={(rating) => setRating(rating)}/>
         <div className="input-group">
           <input onChange={handleTextChange} type="text"
            placeholder="Write a review" 
            value={text}/>
-          <Button type="submit" version='secondary'>Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>Send</Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
